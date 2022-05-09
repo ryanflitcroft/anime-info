1d2acb1e889ca31c8f8ab5fe8760b53791268290
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-05-09 14:46:37 -0700

    init components Header, Main, Footer. Init Routes in Main.

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+export default function Main() {
+  return (
+    <>
+      <main>
+        <Switch>
+          <Route path="/"></Route>
+        </Switch>
+      </main>
+    </>
+  );
+}
