ffc83a239e7be241b97d12538b0748a60ee10724
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-05-09 14:52:37 -0700

    init msw in setupTests.

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,2 +1,11 @@
+import { setupServer } from 'msw/node';
+import { rest } from 'msw';
+
 import fetch from 'cross-fetch';
 global.fetch = fetch;
+
+const server = setupServer(rest.get('', (req, res, ctx) => res(ctx.json(''))));
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
